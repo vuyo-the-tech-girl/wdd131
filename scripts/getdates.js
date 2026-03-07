ffc6380889cb81f88c53bfb0b471cf4f07c9a04b
vuyo-the-tech-girl <vfaleni@byupathway.edu>
2026-03-08 00:26:23 +0200

    js update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',() => {
     const currentYear = new Date ().getFullYear();
-    document.getElementById('currentyear').innerHTML = currentYear;
+    document.getElementById('currentyear').innerHTML = currentYear
 
     document.getElementById('lastModified').innerHTML = document.lastModified
 });
