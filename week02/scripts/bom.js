a83f9affc5cfcf7f603f4baa4a8ecdaf2f44fee8
vuyo-the-tech-girl <vfaleni@byupathway.edu>
2026-03-12 20:57:00 +0200

    learning activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,20 @@
+
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
+buttonElement.addEventListener("click", function () {
+if (inputElement.value != "") {
+const li = document.createElement("li");
+li.textContent = inputElement.value;
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "❌";
+deleteBtn.addEventListener("click", function () {
+listElement.removeChild(li);
+inputElement.focus();
+});
+li.appendChild(deleteBtn);
+listElement.appendChild(li);
+inputElement.value = "";
+}
+inputElement.focus();
+});
